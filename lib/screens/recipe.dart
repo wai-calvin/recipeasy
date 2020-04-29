170f5daeacb7794b1803a7bf3f72dd3cb4632f6b
wai-calvin <cwai14@live.com>
2020-04-28 22:05:51 -0700

    search by recipe base code

--- lib/screens/recipe.dart
+++ lib/screens/recipe.dart
@@ -1,60 +1,56 @@
-//import 'package:flutter/material.dart';
-//import 'package:recipeasy/services/api_services.dart';
-//import 'package:webview_flutter/webview_flutter.dart';
-//
-//class SearchRecipePage extends StatelessWidget {
-//  const SearchRecipePage({Key key}) : super(key: key);
-//
-//  void _getRandomRecipe(BuildContext context) async {
-//    var recipe = await ApiService.instance.randomRecipe();
-//    Navigator.push(
-//      context,
-//      MaterialPageRoute(builder: (context) => SearchRecipe(recipe: recipe)),
-//    );
-//    //    print(recipe['spoonacularSourceUrl']);
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("RECIPEASY"),
-//        centerTitle: true,
-//      ),
-//      body: Text("Get Random Recipe"),
-//      floatingActionButton: FloatingActionButton.extended(
-//        onPressed: (){
-//          _getRandomRecipe(context);
-//        },
-//        label: Text('Get Random Recipe'),
-//      ),
-//      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-//    );
-//  }
-//}
-//
-//class SearchRecipe extends StatefulWidget {
-//  //It returns a final mealPlan variable
-//  final Map<String,dynamic> recipe;
-//  SearchRecipe({this.recipe});
-//
-//  @override
-//  _SearchRecipeState createState() => _SearchRecipeState();
-//}
-//
-//class _SearchRecipeState extends State<SearchRecipe> {
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("RECIPEASY"),
-//        centerTitle: true,
-//      ),
-//      body: WebView(
-//        initialUrl: widget.recipe['spoonacularSourceUrl'],
-//        //JS unrestricted, so that JS can execute in the webview
-//        javascriptMode: JavascriptMode.unrestricted,
-//      ),
-////      Text(widget.recipe['spoonacularSourceUrl']),
-//    );
-//  }
-//}
+import 'package:flutter/material.dart';
+import 'package:recipeasy/services/api_services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class RecipePage extends StatelessWidget {
+  //const RecipePage({Key key}) : super(key: key);
+  final myController = TextEditingController();
+
+  void _getRecipe(BuildContext context, String input) async {
+    var recipe = await ApiService.instance.findRecipe(input);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FindRecipe(recipe: recipe)),
+    );
+    //    print(recipe['spoonacularSourceUrl']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("RECIPEASY"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          _getRecipe(context, myController.text);
+        },
+        label: Text('Get Random Recipe'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class FindRecipe extends StatefulWidget {
+  final Map<String, dynamic> recipe;
+  FindRecipe({this.recipe});
+
+  @override
+  _FindRecipeState createState() => _FindRecipeState();
+}
+
+class _FindRecipeState extends State<FindRecipe> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
