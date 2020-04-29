a52e5c6df04959696845795aeb0e13a5b20a212b
wai-calvin <cwai14@live.com>
2020-04-28 22:21:56 -0700

    better working search by recipe base

--- lib/screens/recipe.dart
+++ lib/screens/recipe.dart
@@ -2,55 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:recipeasy/services/api_services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class RecipePage extends StatelessWidget {
-  //const RecipePage({Key key}) : super(key: key);
+class RecipePage extends StatefulWidget {
+  const RecipePage({Key key}) : super(key: key);
+
+  @override
+  _RecipePageState createState() => _RecipePageState();
+}
+
+class _RecipePageState extends State<RecipePage> {
   final myController = TextEditingController();
 
-  void _getRecipe(BuildContext context, String input) async {
+   _getRecipe(BuildContext context, String input) async {
     var recipe = await ApiService.instance.findRecipe(input);
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => FindRecipe(recipe: recipe)),
+      MaterialPageRoute(builder: (context) => RecipePage()),
     );
-    //    print(recipe['spoonacularSourceUrl']);
   }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("RECIPEASY"),
+        title: Text('Search Recipes'),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: TextField(
           controller: myController,
-        ),
+        )
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: (){
           _getRecipe(context, myController.text);
         },
-        label: Text('Get Random Recipe'),
+        label: Text('Search Recipes'),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  }
-}
-
-class FindRecipe extends StatefulWidget {
-  final Map<String, dynamic> recipe;
-  FindRecipe({this.recipe});
-
-  @override
-  _FindRecipeState createState() => _FindRecipeState();
-}
-
-class _FindRecipeState extends State<FindRecipe> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
   }
 }
